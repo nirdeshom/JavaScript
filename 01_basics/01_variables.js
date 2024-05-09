40634dee72fa73589684f0dc74a8bfbc2b6874b9
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-05-09 18:41:25 +0530

    add file 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const myId = 19222535
+let myEmail = "nir@gmail.com";
+var myPassword = "Ni@312";
+myBranch = 'Information Technology';
+let myCity;
+
+// myId = 1234  // not allowed
+
+myEmail = "nir@google.com";
+console.log(myId);
+console.table([myId, myEmail, myPassword, myBranch, myCity]);
+
+/*
+1=> prefer not to use var
+because of issue in block scope and functional scope
+
+2=>  variable is a container for storing data values. Variables are used to hold information that can be referenced and manipulated in a program.
+
+3=> When you declare a variable in JavaScript, you use keywords like var, let, or const
+
+4=> introduced in ES6, const is used to declare variables their value cannot be reassigned once initialized.
+*/
