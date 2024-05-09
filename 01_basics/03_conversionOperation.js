709083b9f727372390fc935ee7037436b236fb86
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-05-10 00:33:28 +0530

    convertion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "99abc";
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let strNumber = Number(score)
+
+// console.log(strNumber);
+// console.log(typeof strNumber);
+
+// "99" => 99
+// "99abc" => NaN 
+// true => 1, false => 0
+
+let isLoggedIn = 0;
+
+let numBolleane = Boolean(isLoggedIn);
+console.log(isLoggedIn);
+console.log(numBolleane);
+
+// 1 => true, 0 => false
+// "" => false
+// "Nirdesh" => true
