08a16d72d09f89f8de018755cc480f4a1f4e5bd0
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-05-10 11:49:10 +0530

    Arithmetic Operators

---
+++ operators/01_arithmetic.js
@@ -0,0 +1,10 @@
+// Arithmetic Operators: Perform arithmetic operations on numerical values
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
