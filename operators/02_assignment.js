0537622f1cd28bc250357ee4261a83c6535267ec
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-05-15 14:58:41 +0530

    Assignment Operators

---
+++ operators/02_assignment.js
@@ -0,0 +1,12 @@
+// Assignment Operators: Assign values to variables.
+
+let a = 6;
+
+// a = a + 5      //  a = a + 5
+// a += 5        //  a = a + 5
+// a -= 5       //  a = a - 5
+// a /= 5      //  a = a / 5
+// a *= 5     //  a = a * 5
+a %= 5       //  a = a % 5
+
+console.log(a);
