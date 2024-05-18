9d6aaace933a5d79bc36746c99e50da6895687ea
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-05-18 15:07:43 +0530

    Comparison Operators

---
+++ operators/03_comparison.js
@@ -0,0 +1,20 @@
+// JavaScript comparison operators are used to check if a condition is true or false. The comparison operators are used in logical expressions to determine their equality or differences in variables or values.
+
+
+console.log(3 > 5);
+console.log(3 < 5);
+console.log(3 >= 5);
+console.log(3 <= 5);
+
+console.log(3 === 5);
+console.log(3 === 3);
+console.log("3" == 3);
+console.log("3" === 3);
+console.log("Nirdesh" === "Nirdesh");
+
+console.log(3 !== 3);
+console.log(2 !== 3);
+
+
+
+
