83fa1b4785ff7c900d674580c34d446f3ad2e834
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-05-25 05:37:42 +0530

    IIFE Named and IIFE Arrow

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately invoked function Expression (IIFE)
+
+// named iife 
+(function om(){
+    console.log("Named IIFE")
+
+})();
+
+
+// Arrow IIFE 
+((name) => {
+    console.log(`hello iife ${name}`)
+})("nirdesh");
+
+
