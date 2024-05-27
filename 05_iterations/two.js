384d9b6624f2881c75154b6aaa5745c959acd53c
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2024-05-27 14:53:33 +0530

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// let index = 1
+
+// while (index <= 10) {
+//     console.log(index);
+//     index = index + 2
+// }
+
+
+let myArr = ["nirdesh", "omjeet", "vimal"];
+
+let arr = 0;
+while(arr < myArr.length){
+    // console.log(`value is ${myArr[arr]}`);
+    arr = arr + 1;
+}
+
+
+let score = 1;
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 5);
+
