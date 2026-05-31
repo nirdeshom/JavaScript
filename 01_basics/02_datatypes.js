2b9706cb92e9168233ee76dcffa097d063e05c8b
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-05-31 17:28:58 +0530

    data type and typeof

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all js code as newer version
+
+let name = "nirdest";
+let age = 23;
+let state;
+let city = null;
+
+// number = 2 topower 53
+// bigInt =
+// string = "", '', ``
+// boolean = true/false
+// null = standalone value
+// undefined =
+// Symbol = unique
+
+// object
+
+console.log(typeof "nirdesh");
+console.log(typeof age);
+console.log(typeof (state));  // undefined
+console.log(typeof null);  // object
