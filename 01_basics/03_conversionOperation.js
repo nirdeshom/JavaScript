bfeaf4ef9826d4e49f4d81450c3c2e1d0eaf4851
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-06-01 18:18:24 +0530

    data type basic

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "99";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+// "99" = number
+// "99abc" = NaN (Not a Number)
+//  true = 1 false = 0
+
+let isLoggedIn = "hitesh";
+
+console.log(typeof isLoggedIn);
+
+let valueBoolean = Boolean(isLoggedIn);
+console.log(typeof valueBoolean);
+console.log(valueBoolean);
+
+// 1 = true / 0 = false 
+// "" = false / "nirdesh" = true
