89e0c445bc745a9b04540c5230b30a1995e7243f
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-06-05 12:03:08 +0530

    do-while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,35 @@
+let index = 1;
+
+while(index <= 10){
+    // console.log(`index value of ${index}`);
+    index = index + 1;
+}
+
+const fruits = [
+  "Apple", 
+  "Banana", 
+  "Mango", 
+  "Orange", 
+  "Grapes", 
+  "Papaya", 
+  "Watermelon", 
+  "Pineapple", 
+  "Pomegranate", 
+  "Strawberry"
+];
+let i = 0;
+
+while(i < fruits.length){
+    // console.log(fruits[i]);
+    i = i + 1;
+}
+
+
+// let score = 1;
+let score = 11;
+
+do{
+    console.log(`score ${score}`);
+    score++;
+}
+while(score <= 10)
