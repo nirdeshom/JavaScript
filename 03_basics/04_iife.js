a08b68d7f9950d4755cb81c9d73618b4917a8407
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-06-05 11:54:53 +0530

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// IIFE (Immediately Invoked Function Expression)
+
+(function chai(){
+    // named IIFE
+    console.log("DB CONNECTED");
+})();
+
+(() =>{
+    console.log("DB CONNECTED TWO");
+})()
