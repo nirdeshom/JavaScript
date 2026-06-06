e30ae876389a6158a03182607d68ba2a7e51fa7b
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2026-06-06 12:03:05 +0530

    for in

---
+++ 05_iteration/four.js
@@ -0,0 +1,18 @@
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "swift by apple",
+}
+
+for (const key in myObject) {
+    // console.log(key);
+    // console.log(myObject[key]);
+}
+
+const myArr = ["java", "js", "php", "html", "css"];
+
+for (const key in myArr) {
+    // console.log(key);
+    console.log(myArr[key]);
+}
